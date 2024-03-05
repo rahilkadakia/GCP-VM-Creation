58e2d99fd845be3730871f74ccb2ce0829754d65
Rahil Kadakia <rahilkadakia99@gmail.com>
2024-03-05 16:47:04 -0500

    Cleaned code

--- rnk9684_HW2.py
+++ rnk9684_HW2.py
@@ -12,16 +12,6 @@ from google.cloud import compute_v1
 
 
 def get_image_from_family(project: str, family: str) -> compute_v1.Image:
-    """
-    Retrieve the newest image that is part of a given family in a project.
-
-    Args:
-        project: project ID or project number of the Cloud project you want to get image from.
-        family: name of the image family you want to get image from.
-
-    Returns:
-        An Image object.
-    """
     image_client = compute_v1.ImagesClient()
     # List of public operating system (OS) images: https://cloud.google.com/compute/docs/images/os-details
     newest_image = image_client.get_from_family(project=project, family=family)
@@ -35,24 +25,7 @@ def disk_from_image(
     source_image: str,
     auto_delete: bool = True,
 ) -> compute_v1.AttachedDisk:
-    """
-    Create an AttachedDisk object to be used in VM instance creation. Uses an image as the
-    source for the new disk.
-
-    Args:
-         disk_type: the type of disk you want to create. This value uses the following format:
-            "zones/{zone}/diskTypes/(pd-standard|pd-ssd|pd-balanced|pd-extreme)".
-            For example: "zones/us-west3-b/diskTypes/pd-ssd"
-        disk_size_gb: size of the new disk in gigabytes
-        boot: boolean flag indicating whether this disk should be used as a boot disk of an instance
-        source_image: source image to use when creating this disk. You must have read access to this disk. This can be one
-            of the publicly available images or an image from one of your projects.
-            This value uses the following format: "projects/{project_name}/global/images/{image_name}"
-        auto_delete: boolean flag indicating whether this disk should be deleted with the VM that uses it
-
-    Returns:
-        AttachedDisk object configured to be created using the specified image.
-    """
+    
     boot_disk = compute_v1.AttachedDisk()
     initialize_params = compute_v1.AttachedDiskInitializeParams()
     initialize_params.source_image = source_image
@@ -69,32 +42,7 @@ def disk_from_image(
 def wait_for_extended_operation(
     operation: ExtendedOperation, verbose_name: str = "operation", timeout: int = 300
 ) -> Any:
-    """
-    Waits for the extended (long-running) operation to complete.
-
-    If the operation is successful, it will return its result.
-    If the operation ends with an error, an exception will be raised.
-    If there were any warnings during the execution of the operation
-    they will be printed to sys.stderr.
-
-    Args:
-        operation: a long-running operation you want to wait on.
-        verbose_name: (optional) a more verbose name of the operation,
-            used only during error and warning reporting.
-        timeout: how long (in seconds) to wait for operation to finish.
-            If None, wait indefinitely.
-
-    Returns:
-        Whatever the operation.result() returns.
-
-    Raises:
-        This method will raise the exception received from `operation.exception()`
-        or RuntimeError if there is no exception set, but there is an `error_code`
-        set for the `operation`.
-
-        In case of an operation taking longer than `timeout` seconds to complete,
-        a `concurrent.futures.TimeoutError` will be raised.
-    """
+   
     result = operation.result(timeout=timeout)
 
     if operation.error_code:
@@ -132,46 +80,7 @@ def create_instance(
     custom_hostname: str = None,
     delete_protection: bool = False,
 ) -> compute_v1.Instance:
-    """
-    Send an instance creation request to the Compute Engine API and wait for it to complete.
-
-    Args:
-        project_id: project ID or project number of the Cloud project you want to use.
-        zone: name of the zone to create the instance in. For example: "us-west3-b"
-        instance_name: name of the new virtual machine (VM) instance.
-        disks: a list of compute_v1.AttachedDisk objects describing the disks
-            you want to attach to your new instance.
-        machine_type: machine type of the VM being created. This value uses the
-            following format: "zones/{zone}/machineTypes/{type_name}".
-            For example: "zones/europe-west3-c/machineTypes/f1-micro"
-        network_link: name of the network you want the new instance to use.
-            For example: "global/networks/default" represents the network
-            named "default", which is created automatically for each project.
-        subnetwork_link: name of the subnetwork you want the new instance to use.
-            This value uses the following format:
-            "regions/{region}/subnetworks/{subnetwork_name}"
-        internal_ip: internal IP address you want to assign to the new instance.
-            By default, a free address from the pool of available internal IP addresses of
-            used subnet will be used.
-        external_access: boolean flag indicating if the instance should have an external IPv4
-            address assigned.
-        external_ipv4: external IPv4 address to be assigned to this instance. If you specify
-            an external IP address, it must live in the same region as the zone of the instance.
-            This setting requires `external_access` to be set to True to work.
-        accelerators: a list of AcceleratorConfig objects describing the accelerators that will
-            be attached to the new instance.
-        preemptible: boolean value indicating if the new instance should be preemptible
-            or not. Preemptible VMs have been deprecated and you should now use Spot VMs.
-        spot: boolean value indicating if the new instance should be a Spot VM or not.
-        instance_termination_action: What action should be taken once a Spot VM is terminated.
-            Possible values: "STOP", "DELETE"
-        custom_hostname: Custom hostname of the new VM instance.
-            Custom hostnames must conform to RFC 1035 requirements for valid hostnames.
-        delete_protection: boolean value indicating if the new virtual machine should be
-            protected against deletion or not.
-    Returns:
-        Instance object.
-    """
+    
     instance_client = compute_v1.InstancesClient()
 
     # Use the network interface provided in the network_link argument.
